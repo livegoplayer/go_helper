8d0599809a74c333d2618278e56ef616747a26c4
bug <2532917800@qq.com>
2020-08-15 15:02:51 +0800

    修改 返回值相关属性

--- util.go
+++ util.go
@@ -183,7 +183,8 @@ func Substring(source string, start int, end int) string {
 //获取两个字符串中间的字符串
 func GetSubStringBetween(source string, startString string, endString string) string {
 	//先拿到第一个字符串到最后的子字符串
-	source = Substring(source, strings.Index(source, startString)+strings.Count(startString, "")-1, strings.Count(source, "")-1)
+	start := Index(source, startString)
+	source = Substring(source, start==-1 ? 0 : start+strings.Count(startString, "")-1, strings.Count(source, "")-1)
 	if endString == "" {
 		return source
 	}
