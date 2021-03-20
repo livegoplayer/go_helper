e748b6374fa18cc4001fd2c202750fe9ed1f1eb4
xjyplayer <2532917800@qq.com>
2021-03-20 16:18:38 +0800

    1.2.5

--- utils/str.go
+++ utils/str.go
@@ -1,5 +1,11 @@
 package utils
 
+import "strings"
+
 func Substring(str string, startIndex int64, endIndex int64) string {
 	return str[startIndex:endIndex]
+}
+
+func GetSubStringBetween(str string, begin string, end string) string {
+	return Substring(str, int64(strings.Index(str, begin)), int64(strings.Index(str, end)))
 }
