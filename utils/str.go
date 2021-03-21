bae59ad90c6a3f5b86751abc80d8c36947282b42
xjyplayer <2532917800@qq.com>
2021-03-21 16:56:09 +0800

    1.2.5

--- utils/str.go
+++ utils/str.go
@@ -2,10 +2,32 @@ package utils
 
 import "strings"
 
-func Substring(str string, startIndex int64, endIndex int64) string {
-	return str[startIndex:endIndex]
+func Substring(source string, start int64, end int64) string {
+	var r = []rune(source)
+	length := int64(len(r))
+
+	if start < 0 || end > length || start > end {
+		return ""
+	}
+
+	if start == 0 && end == length {
+		return source
+	}
+
+	return string(r[start:end])
 }
 
-func GetSubStringBetween(str string, begin string, end string) string {
-	return Substring(str, int64(strings.Index(str, begin)), int64(strings.Index(str, end)))
+//获取两个字符串中间的字符串
+func GetSubStringBetween(source string, startString string, endString string) string {
+	//先拿到第一个字符串到最后的子字符串
+	start := int64(strings.Index(source, startString))
+	startIndex := int64(0)
+	if start != int64(-1) {
+		startIndex = start + int64(strings.Count(startString, "")) - 1
+	}
+	source = Substring(source, startIndex, int64(strings.Count(source, "")-1))
+	if endString == "" {
+		return source
+	}
+	return Substring(source, 0, int64(strings.Index(source, endString)))
 }
