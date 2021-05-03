731122e6171194e893354a9b455a263ed4fad8ff
xjyplayer <2532917800@qq.com>
2021-05-03 15:06:54 +0800

    1.2.6

--- utils/type.go
+++ utils/type.go
@@ -6,25 +6,6 @@ import (
 	"strconv"
 )
 
-func String(unknownVal interface{}) string {
-	var stringVal, ok = unknownVal.(string)
-
-	if ok {
-		return stringVal
-	}
-	return ""
-}
-
-func Int32(unknownVal interface{}) int32 {
-	intVal, ok := unknownVal.(int32)
-
-	if ok {
-		return intVal
-	}
-
-	return 0
-}
-
 func BytesToString(b []byte) string {
 	return string(b)
 }
