b51e9f54dc3441e575b20714e4c3428de011e043
xjyplayer <2532917800@qq.com>
2023-02-14 12:55:24 +0800

    增加函数

--- pagination/page.go
+++ pagination/page.go
@@ -1,12 +1,11 @@
 package pagination
 
 type PageOption struct {
-	Size    int
-	Skip    int
-	Total   int
-	PageNum int
-	Order   string
-	LastId  interface{}
+	Size    int         `json:"size,omitempty"`
+	Skip    int         `json:"skip,omitempty"`
+	Total   int         `json:"total,omitempty"`
+	PageNum int         `json:"page_num,omitempty"`
+	LastId  interface{} `json:"last_id,omitempty"`
 }
 
 func NewPageOption(size, skip int, order string, LastId interface{}) *PageOption {
@@ -20,14 +19,13 @@ func NewPageOption(size, skip int, order string, LastId interface{}) *PageOption
 		Size:   size,
 		Skip:   skip,
 		Total:  0,
-		Order:  order,
 		LastId: LastId,
 	}
 }
 
 func NewNumPage(pageNum, size int) *PageOption {
 	p := PageOption{}
-	p.Size = 10
+	p.Size = 20
 	p.PageNum = pageNum
 	if p.PageNum == 0 {
 		p.PageNum = 1
@@ -35,7 +33,6 @@ func NewNumPage(pageNum, size int) *PageOption {
 	if size > 0 {
 		p.Size = size
 	}
-	p.Order = "ctime"
 	p.Skip = p.Size * (p.PageNum - 1)
 	return &p
 }
--- utils/slice.go
+++ utils/slice.go
@@ -1,1 +1,28 @@
 package utils
+
+// 定义一个新的类型：element
+type element interface {
+	// element 支持如下类型
+	string | int8 | int16 | int32 | int64 | int | float32 | float64 | uint | uint8 | uint16 | uint32 | uint64
+}
+
+// Unique 数组或slice边去重
+func Unique[T element](arr []T) []T {
+	tmp := make(map[T]struct{})
+	l := len(arr)
+	if l == 0 {
+		return arr
+	}
+
+	rel := make([]T, 0, l)
+	for _, item := range arr {
+		_, ok := tmp[item]
+		if ok {
+			continue
+		}
+		tmp[item] = struct{}{}
+		rel = append(rel, item)
+	}
+
+	return rel[:len(tmp)]
+}
